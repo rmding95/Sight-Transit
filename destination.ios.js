7898e43f4d4400155e9bda279fc203a175b71c9c
Richard Ding <rmding2013college@gmail.com>
2017-04-29 15:14:20 -0700

    started work on voice commands and google place api:

--- destination.ios.js
+++ destination.ios.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -5,10 +7,13 @@ import {
   Text,
   View,
   TextInput,
-   Button
+    Button,
+    NativeAppEventEmitter
 } from 'react-native';
 
 var DirectionScreen = require('./direction.ios.js');
+var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
+var SpeechToText = require('react-native-speech-to-text-ios');
 
 // this page allows users to input their destination
 // todo: style is a bit off... navigator bar requires margin. smh.
@@ -16,19 +21,58 @@ var DirectionScreen = require('./direction.ios.js');
 
 // i do believe tripentry.ios.js is no longer needed. although,
 // this page isn't necessary either.
+
+//Google Place API Key: AIzaSyBb7lDyjRIlU7STXU8d4WueMDk5bI3sxrU
 class DestinationScreen extends Component {
     constructor(props) {
         super(props);
         this.state = { text: '' };
-  }
+    }
 
    _onPress = (location) => {
-     console.log("pressed on continue button");
-     this.props.navigator.push({
-       title: "Direction",
-       component: DirectionScreen,
-       passProps: {myProp: location}
-     });
+        console.log("pressed on continue button");
+        this.props.navigator.push({
+        title: "Direction",
+        component: DirectionScreen,
+        passProps: {myProp: location}
+        });
+   }
+
+   componentDidMount() {
+       navigator.geolocation.getCurrentPosition(
+            (position) => {
+                var initialPosition = JSON.stringify(position);
+                this.setState({initialPosition});
+            },
+            (error) => alert(error.message),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+       );
+       this.watchID = navigator.geolocation.watchPosition((position) => {
+            var lastPosition = JSON.stringify(position);
+            this.setState({lastPosition});
+        });
+
+        this.subscription = NativeAppEventEmitter.addListener(
+            'SpeechToText',
+            (result) => {
+                if (result.error) {
+                    alert(JSON.stringify(result.error));
+                } else {
+                    console.log(result.bestTranscription.formattedString);
+                }        
+            }
+        );
+
+        SpeechToText.startRecognition("en-US");
+   }
+
+   componentWillUnmount() {
+       navigator.geolocation.clearWatch(this.watchID);
+
+        if (this.subscription != null) {
+            this.subscription.remove();
+            this.subscription = null;
+        }
    }
 
     render() {
@@ -36,12 +80,59 @@ class DestinationScreen extends Component {
             <View style={styles.container} accessible={true} accessibilityLabel={'Destination'}>
                 <View>
                     <Text> Where are you headed? </Text>
+                    <Text> {this.state.initialPosition} </Text>
                 </View>
 
-                <TextInput
-                    style={styles.input}
-                    onChangeText={(text) => this.setState({text})}
-                    value={this.state.text}
+                <GooglePlacesAutocomplete
+                    styles={{
+                        textInputContainer: {
+                        borderTopWidth: 50,
+                        borderBottomWidth:0
+                        },
+                        textInput: {
+                        marginLeft: 0,
+                        marginRight: 0,
+
+                        height: 38,
+                        color: '#5d5d5d',
+                        fontSize: 16
+                        },
+                        predefinedPlacesDescription: {
+                        color: '#1faadb'
+                        },
+                    }}
+                    placeholder='Search'
+                    minLength={2}
+                    autoFocus={true}
+                    listViewDisplayed='auto'
+                    fetchDetails={true}
+                    renderDescription={(row) => row.description}
+                    onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                    console.log(data);
+                    console.log(details);
+                    }}
+                    query={{
+                    // available options: https://developers.google.com/places/web-service/autocomplete
+                    key: 'AIzaSyBb7lDyjRIlU7STXU8d4WueMDk5bI3sxrU',
+                    language: 'en', // language of the results
+                    types: '(geocode)', // default: 'geocode'
+                    }}
+                    styles={{
+                    description: {
+                        fontWeight: 'bold',
+                    },
+                    predefinedPlacesDescription: {
+                        color: '#1faadb',
+                    },
+                    }}
+
+                    currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
+                    currentLocationLabel="Current location"
+                    nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+                    GooglePlacesSearchQuery={{
+                    // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+                    rankby: 'distance'
+                    }}
                 />
 
                 <Button
@@ -59,7 +150,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        marginTop: 100
     },
     txt: {
         flex: 0.5
