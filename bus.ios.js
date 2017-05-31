ba653c592799bb8b3520556d71bfdca6bd9e5f24
whydang <whydang@uw.edu>
2017-05-31 02:04:26 -0700

    press and hold to voice

--- bus.ios.js
+++ bus.ios.js
@@ -18,33 +18,49 @@ var BusArrivalScreen = require('./busarrival.ios.js');
 // we need to work on this page since the OBA API has not been called yet
 class BusInformationScreen extends Component {
 
-  constructor(props) {
-    super(props);
-    var transitDetails = props.currentDirection.transitDetails;
-    this.state = {
-        currentDirection: props.currentDirection,
-        routeDetails: props.routeDetails,
-        transitDetails: transitDetails,
-        destinationName: props.destinationName,
-        startAddress: props.startAddress
-    };
-  }
+    constructor(props) {
+        super(props);
+        var transitDetails = props.currentDirection.transitDetails;
+        this.state = {
+            currentDirection: props.currentDirection,
+            routeDetails: props.routeDetails,
+            transitDetails: transitDetails,
+            destinationName: props.destinationName,
+            startAddress: props.startAddress,
+            timer: 10
+        };
+    }
 
-  componentWillMount() {
-      // const uuid = "e20a39f4-73f5-4bc4-a12f-17d1ad07a961"; // this is the bus_stop's
-      const uuid = "2d3d4821-9c64-4b3d-81d6-5b3d9ba6eab1"; // this is the phone's uuid... in real world need to create a unique one per DL
-      const identifier = "user_beacon";
-      const major = 0;
-      const minor = 256;
-      BeaconBroadcast.startAdvertisingBeaconWithString(uuid, identifier, major, minor);
-  }
+    componentWillMount() {
+        // const uuid = "e20a39f4-73f5-4bc4-a12f-17d1ad07a961"; // this is the bus_stop's
+        const uuid = "2d3d4821-9c64-4b3d-81d6-5b3d9ba6eab1"; // this is the phone's uuid... in real world need to create a unique one per DL
+        const identifier = "user_beacon";
+        const major = 0;
+        const minor = 256;
+        BeaconBroadcast.startAdvertisingBeaconWithString(uuid, identifier, major, minor);
+    }
 
-    _onPress = () => {
-        this.props.navigator.replace({
-            title: "Bus Arrival",
-            component: BusArrivalScreen,
-            passProps: {currentDirection: this.state.currentDirection, routeDetails: this.state.routeDetails, transitDetails: this.state.transitDetails, destinationName: this.state.destinationName, startAddress: this.state.startAddress}
+    componentDidMount() {
+        var timerID = setInterval( () => {
+            this.setState({
+                timer: this.state.timer - 1
+            })
+            }, 1000)
+        this.setState({
+            timerID: timerID
         });
+    }
+
+    componentDidUpdate() {
+        if (this.state.timer < 1) {
+            clearInterval(this.state.timerID);
+            
+            this.props.navigator.replace({
+                title: "Bus Arrival",
+                component: BusArrivalScreen,
+                passProps: {currentDirection: this.state.currentDirection, routeDetails: this.state.routeDetails, transitDetails: this.state.transitDetails, destinationName: this.state.destinationName, startAddress: this.state.startAddress}
+            });
+        }
     }
 
     render() {
@@ -54,12 +70,6 @@ class BusInformationScreen extends Component {
                     <Text style={styles.details}>Your next bus {this.state.transitDetails.line.short_name}</Text>
                     <Text style={styles.details}>Arrives in {this.state.transitDetails.arrivalTime.text} minutes</Text>
                 </View>
-                
-                <Button
-                    onPress={() => this._onPress()}
-                    title="Continue"
-                    accessibilityLabel="Continue"
-                />
             </View>
         );
     }
